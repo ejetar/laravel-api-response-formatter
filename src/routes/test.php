67b9ff3747e37b9d59d320cd740e32f25a3b3d8f
Guilherme Almeida Girardi <guilhermeagirardi@gmail.com>
2019-07-31 20:09:32 -0300

    /test route ren. to /api-response-formatter-test
    ...

--- src/routes/test.php
+++ src/routes/test.php
@@ -1,4 +1,4 @@
 <?php
-Route::middleware(['response_formatter'])->get('test', function () {
+Route::middleware(['response_formatter'])->get('api-response-formatter-test', function () {
     return API_RESPONSE_FORMATTER_ASSERT_ARRAY;
 });
